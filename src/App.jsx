4102629ea218662c79bdbf6a7596de8476fbab2a
srjoy5000 <riosato2000@gmail.com>
2026-04-17 14:11:04 +0900

    fix: renew API source for face detection

--- src/App.jsx
+++ src/App.jsx
@@ -1,150 +1,157 @@
-import { Component } from 'react'
-import Navigation from './components/navigation/Navigation'
-import Logo from './components/logo/Logo'
-import Rank from './components/rank/Rank'
-import ImageLinkForm from './components/imagelinkform/ImageLinkForm'
-import FaceRecognition from './components/facerecognition/FaceRecognition'
-import SignIn from './components/SignIn/SignIn'
-import Register from './components/Register/Register'
-import ParticlesBg from 'particles-bg'
-import './App.css'
+import { Component } from "react";
+import { FaceDetector, FilesetResolver } from "@mediapipe/tasks-vision";
+import Navigation from "./components/navigation/Navigation";
+import Logo from "./components/logo/Logo";
+import Rank from "./components/rank/Rank";
+import ImageLinkForm from "./components/imagelinkform/ImageLinkForm";
+import FaceRecognition from "./components/facerecognition/FaceRecognition";
+import SignIn from "./components/SignIn/SignIn";
+import Register from "./components/Register/Register";
+import ParticlesBg from "particles-bg";
+import "./App.css";
 
-const baseURL = 'https://ai-face-recognition-api.onrender.com/'
+const baseURL = "https://ai-face-recognition-api.onrender.com/";
 
 const initState = {
-  input: '',
-  imgURL: '',
+  input: "",
+  imgURL: "",
   bboxes: [],
-  route: 'signIn',
+  route: "signIn",
   isSignedIn: false,
   user: {
-    id: '',
-    name: '',
-    email: '',
+    id: "",
+    name: "",
+    email: "",
     entries: 0,
-    joined: ''
-  }
-}
+    joined: "",
+  },
+};
 
 class App extends Component {
   constructor() {
-    super()
-    this.state = initState
+    super();
+    this.state = initState;
   }
 
   loadUser = (userData) => {
-    const { id, name, email, entries, joined } = userData
+    const { id, name, email, entries, joined } = userData;
     this.setState({
       user: {
         id: id,
         name: name,
         email: email,
         entries: entries,
-        joined: joined
-      }
-    })
-  }
+        joined: joined,
+      },
+    });
+  };
 
   faceLocation = (data) => {
-    console.log(data)
-    const regions = data.outputs[0].data.regions
-    const image = document.getElementById("inputImage")
-    const width = Number(image.width)
-    const height = Number(image.height)
+    console.log(data);
+    const regions = data.outputs[0].data.regions;
+    const image = document.getElementById("inputImage");
+    const width = Number(image.width);
+    const height = Number(image.height);
 
-    const bboxes = regions.map(region => {
-      const boundingBox = region.region_info.bounding_box
+    const bboxes = regions.map((region) => {
+      const boundingBox = region.region_info.bounding_box;
 
       return {
         leftCol: boundingBox.left_col * width,
         topRow: boundingBox.top_row * height,
-        rightCol: width - (boundingBox.right_col * width),
-        bottomRow: height - (boundingBox.bottom_row * height),
-      }
+        rightCol: width - boundingBox.right_col * width,
+        bottomRow: height - boundingBox.bottom_row * height,
+      };
 
       // region.data.concepts.forEach(concept => {
       //   // Accessing and rounding the concept value
       //   const name = concept.name;
       //   const value = concept.value.toFixed(4);
       // });
-    })
-    return bboxes
-  }
+    });
+    return bboxes;
+  };
 
   displayBBox = (bboxes) => {
-    this.setState({ bboxes: bboxes })
-  }
+    this.setState({ bboxes: bboxes });
+  };
 
   onInputChange = (event) => {
-    this.setState({ input: event.target.value })
-  }
+    this.setState({ input: event.target.value });
+  };
 
   onSubmitButton = async () => {
-    this.setState({ imgURL: this.state.input })
-    fetch(baseURL + 'imageurl', {
-      method: 'post',
-      headers: { 'Content-Type': 'application/json' },
+    this.setState({ imgURL: this.state.input });
+    fetch(baseURL + "imageurl", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
-        input: this.state.input
-      })
+        input: this.state.input,
+      }),
     })
-      .then(response => response.json())
-      .then(result => {
+      .then((response) => response.json())
+      .then((result) => {
         if (result) {
-          fetch(baseURL + 'image', {
-            method: 'put',
-            headers: { 'Content-Type': 'application/json' },
+          fetch(baseURL + "image", {
+            method: "put",
+            headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
-              id: this.state.user.id
-            })
+              id: this.state.user.id,
+            }),
           })
-            .then(response => response.json())
-            .then(count => {
-              this.setState
-                (
-                  Object.assign(this.state.user, { entries: count })
-                )
+            .then((response) => response.json())
+            .then((count) => {
+              this.setState(Object.assign(this.state.user, { entries: count }));
             })
-            .catch(console.log)
+            .catch(console.log);
         }
-        this.displayBBox(this.faceLocation(result))
+        this.displayBBox(this.faceLocation(result));
       })
-      .catch(error => console.log('error', error));
-  }
+      .catch((error) => console.log("error", error));
+  };
 
   onRouteChange = (page) => {
-    if (page !== 'home') {
-      this.setState(initState)
-    } else if (page === 'home') {
-      this.setState({ isSignedIn: true })
+    if (page !== "home") {
+      this.setState(initState);
+    } else if (page === "home") {
+      this.setState({ isSignedIn: true });
     }
-    this.setState({ route: page })
-  }
+    this.setState({ route: page });
+  };
 
   render() {
-    const { isSignedIn, route, bboxes, imgURL, user } = this.state
+    const { isSignedIn, route, bboxes, imgURL, user } = this.state;
     return (
-      <div className='App'>
+      <div className="App">
         <ParticlesBg color="#ffffff" type="cobweb" bg={true} />
-        <Navigation onRouteChange={this.onRouteChange} isSignedIn={isSignedIn} />
-        {route === 'home'
-          ? <>
+        <Navigation
+          onRouteChange={this.onRouteChange}
+          isSignedIn={isSignedIn}
+        />
+        {route === "home" ? (
+          <>
             <Logo />
             <Rank name={user.name} entries={user.entries} />
-            <ImageLinkForm onInputChange={this.onInputChange} onSubmitButton={this.onSubmitButton} />
+            <ImageLinkForm
+              onInputChange={this.onInputChange}
+              onSubmitButton={this.onSubmitButton}
+            />
             <FaceRecognition bboxes={bboxes} imgURL={imgURL} />
           </>
-          : (route === 'signIn'
-            ? <SignIn loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
-            : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
-          )
-        }
+        ) : route === "signIn" ? (
+          <SignIn loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
+        ) : (
+          <Register
+            loadUser={this.loadUser}
+            onRouteChange={this.onRouteChange}
+          />
+        )}
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
 
 /* To-Dos
 - add loading screen when signing in or registering
