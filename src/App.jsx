be71ee56671d34197faf2424a26c6c2cefbb45b3
srjoy5000 <riosato2000@gmail.com>
2026-04-17 23:04:39 +0900

    fix: add github actions for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -31,6 +31,7 @@ class App extends Component {
   constructor() {
     super();
     this.state = initState;
+    this.faceDetector = null;
   }
 
   loadUser = (userData) => {
@@ -46,30 +47,76 @@ class App extends Component {
     });
   };
 
-  faceLocation = (data) => {
-    console.log(data);
-    const regions = data.outputs[0].data.regions;
-    const image = document.getElementById("inputImage");
-    const width = Number(image.width);
-    const height = Number(image.height);
+  getOrCreateFaceDetector = async () => {
+    if (this.faceDetector) return this.faceDetector;
+    const vision = await FilesetResolver.forVisionTasks(
+      "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm",
+    );
+    //define model asset path here. More models can be found here: https://ai.google.dev/edge/mediapipe/solutions/vision/face_detector#models
+    const modelAssetPath =
+      "https://storage.googleapis.com/mediapipe-models/face_detector/blaze_face_full_range/float16/1/blaze_face_full_range.tflite";
 
-    const bboxes = regions.map((region) => {
-      const boundingBox = region.region_info.bounding_box;
-
-      return {
-        leftCol: boundingBox.left_col * width,
-        topRow: boundingBox.top_row * height,
-        rightCol: width - boundingBox.right_col * width,
-        bottomRow: height - boundingBox.bottom_row * height,
-      };
-
-      // region.data.concepts.forEach(concept => {
-      //   // Accessing and rounding the concept value
-      //   const name = concept.name;
-      //   const value = concept.value.toFixed(4);
-      // });
+    this.faceDetector = await FaceDetector.createFromOptions(vision, {
+      baseOptions: {
+        modelAssetPath,
+        delegate: "GPU",
+      },
+      runningMode: "IMAGE",
     });
-    return bboxes;
+    return this.faceDetector;
+  };
+
+  // faceLocation = (data) => {
+  //   console.log(data);
+  //   const regions = data.outputs[0].data.regions;
+  //   const image = document.getElementById("inputImage");
+  //   const width = Number(image.width);
+  //   const height = Number(image.height);
+
+  //   const bboxes = regions.map((region) => {
+  //     const boundingBox = region.region_info.bounding_box;
+
+  //     return {
+  //       leftCol: boundingBox.left_col * width,
+  //       topRow: boundingBox.top_row * height,
+  //       rightCol: width - boundingBox.right_col * width,
+  //       bottomRow: height - boundingBox.bottom_row * height,
+  //     };
+
+  //     // region.data.concepts.forEach(concept => {
+  //     //   // Accessing and rounding the concept value
+  //     //   const name = concept.name;
+  //     //   const value = concept.value.toFixed(4);
+  //     // });
+  //   });
+  //   return bboxes;
+  // };
+
+  calcFaceLocation = async (imageUrl) => {
+    const img = new Image();
+    img.crossOrigin = "anonymous";
+    await new Promise((resolve, reject) => {
+      img.onload = resolve;
+      img.onerror = reject;
+      img.src = imageUrl;
+    });
+
+    const faceDetector = await this.getOrCreateFaceDetector();
+    const result = faceDetector.detect(img);
+
+    const renderedWidth = 500;
+    const scaleFactor = renderedWidth / img.naturalWidth;
+    const renderedHeight = img.naturalHeight * scaleFactor;
+
+    return result.detections.map((detection) => {
+      const { originX, originY, width, height } = detection.boundingBox;
+      return {
+        leftCol: originX * scaleFactor,
+        topRow: originY * scaleFactor,
+        rightCol: renderedWidth - (originX + width) * scaleFactor,
+        bottomRow: renderedHeight - (originY + height) * scaleFactor,
+      };
+    });
   };
 
   displayBBox = (bboxes) => {
@@ -80,34 +127,55 @@ class App extends Component {
     this.setState({ input: event.target.value });
   };
 
-  onSubmitButton = async () => {
+  // onSubmitButton = async () => {
+  //   this.setState({ imgURL: this.state.input });
+  //   fetch(baseURL + "imageurl", {
+  //     method: "post",
+  //     headers: { "Content-Type": "application/json" },
+  //     body: JSON.stringify({
+  //       input: this.state.input,
+  //     }),
+  //   })
+  //     .then((response) => response.json())
+  //     .then((result) => {
+  //       if (result) {
+  //         fetch(baseURL + "image", {
+  //           method: "put",
+  //           headers: { "Content-Type": "application/json" },
+  //           body: JSON.stringify({
+  //             id: this.state.user.id,
+  //           }),
+  //         })
+  //           .then((response) => response.json())
+  //           .then((count) => {
+  //             this.setState(Object.assign(this.state.user, { entries: count }));
+  //           })
+  //           .catch(console.log);
+  //       }
+  //       this.displayBBox(this.faceLocation(result));
+  //     })
+  //     .catch((error) => console.log("error", error));
+  // };
+
+  onButtonSubmit = async () => {
     this.setState({ imgURL: this.state.input });
-    fetch(baseURL + "imageurl", {
-      method: "post",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        input: this.state.input,
-      }),
-    })
-      .then((response) => response.json())
-      .then((result) => {
-        if (result) {
-          fetch(baseURL + "image", {
-            method: "put",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({
-              id: this.state.user.id,
-            }),
-          })
-            .then((response) => response.json())
-            .then((count) => {
-              this.setState(Object.assign(this.state.user, { entries: count }));
-            })
-            .catch(console.log);
-        }
-        this.displayBBox(this.faceLocation(result));
+    try {
+      const boxes = await this.calcFaceLocation(this.state.input);
+      this.displayFaceBox(boxes);
+      fetch(`${baseURL}/image`, {
+        method: "put",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          id: this.state.user.id,
+        }),
       })
-      .catch((error) => console.log("error", error));
+        .then((res) => res.json())
+        .then((count) => {
+          this.setState(Object.assign(this.state.user, { entries: count }));
+        });
+    } catch (error) {
+      console.log("error", error);
+    }
   };
 
   onRouteChange = (page) => {
